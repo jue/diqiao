c67dc06bf5eaa1e392ae58e6d70ac13fbaacfab0
jue <i@xiangjianfeng.com>
2025-04-14 14:56:56 +0800

    更新 package-lock.json 文件，提升 lockfile 版本至 3，并优化 Video.vue 组件的代码格式，确保视频链接的有效性。

--- components/index/Video.vue
+++ components/index/Video.vue
@@ -3,7 +3,7 @@
     <div :key="index" class="items" v-for="(item, index) in videoList">
       <div class="title">
         <i :class="item.icon" class="iconfont"></i>
-        <span>{{item.title}}</span>
+        <span>{{ item.title }}</span>
       </div>
       <div @click="showVideo(item.vurl)" class="img">
         <img :src="item.imgSrc" />
@@ -16,7 +16,7 @@
 import OnlineVideo from '@/components/OnlineVideo.vue'
 export default {
   components: {
-    OnlineVideo
+    OnlineVideo,
   },
   data() {
     return {
@@ -26,22 +26,24 @@ export default {
           title: '专家科普 Technical expert',
           icon: 'icon4',
           imgSrc: '/imgs/index/video001.png',
-          vurl: '/zj.mp4'
+          vurl: '/zj.mp4',
         },
         {
           title: '品牌故事 Brand Story',
           icon: 'icon6',
           imgSrc: '/imgs/index/video002.png',
-          vurl: 'http://api.diqiao.com.cn/attachment/diqiao/diqiao3.mp4'
-        }
-      ]
+          vurl: 'http://api.diqiao.com.cn/attachment/diqiao/diqiao3.mp4',
+        },
+      ],
     }
   },
   methods: {
     showVideo(vurl) {
-      this.$refs.video.show(vurl)
-    }
-  }
+      if (!vurl.includes('http')) {
+        this.$refs.video.show(vurl)
+      }
+    },
+  },
 }
 </script>
 <style lang="less" scoped>
